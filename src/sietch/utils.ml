9e61245fb8fe523bfcc68329c86d469d8ba5d731
mefyl <mefyl@gruntech.org>
2020-05-01 11:41:46 +0200

    Add Barrier.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/sietch/utils.ml
+++ src/sietch/utils.ml
@@ -45,3 +45,57 @@ module LwtrO = struct
 
   let ( let+ ) = ( >|= )
 end
+
+(** A barrier that let thread throughs after it has been opened for some
+    duration. Enables to postpone low priority operations until main operations
+    have been silent for some time. *)
+module Barrier = struct
+  open LwtO
+
+  type t =
+    { mutable open_ : bool
+    ; mutable time : float
+    ; mutable promise : unit Lwt.t
+    ; mutable resolve : unit Lwt.u
+    ; threshold : float
+    }
+
+  let make ?(threshold = 0.1) () =
+    let promise, resolve = Lwt.wait () in
+    { open_ = true; time = Unix.gettimeofday (); promise; resolve; threshold }
+
+  let open_ = function
+    | { open_ = true; _ } -> ()
+    | { resolve; _ } as b ->
+      let p, r = Lwt.wait () in
+      b.open_ <- true;
+      b.time <- Unix.gettimeofday ();
+      b.promise <- p;
+      b.resolve <- r;
+      Lwt.wakeup resolve ()
+
+  let close b = b.open_ <- false
+
+  let rec wait = function
+    | { open_ = true; time; _ } as b ->
+      let now = Unix.gettimeofday () in
+      let elapsed = now -. time in
+      if elapsed > b.threshold then
+        Lwt.return ()
+      else
+        let%lwt () = Lwt_unix.sleep (b.threshold -. elapsed) in
+        wait b
+    | { promise; _ } as b ->
+      let* () = promise in
+      wait b
+
+  let use b f =
+    close b;
+    match%lwt f () with
+    | e ->
+      open_ b;
+      Lwt.return e
+    | exception e ->
+      open_ b;
+      raise e
+end
