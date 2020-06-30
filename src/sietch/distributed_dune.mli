b3b664aec7f35382d7bdb226370ad935abdbe308
mefyl <mefyl@gruntech.org>
2020-06-30 15:44:31 +0200

    Add distributed cache configuration file.
    Signed-off-by: mefyl <mefyl@gruntech.org>

--- src/sietch/distributed_dune.mli
+++ src/sietch/distributed_dune.mli
@@ -1,1 +1,16 @@
+open Stdune
+
+module Config : sig
+  type t = { nodes : node list }
+
+  and node =
+    { hostname : string
+    ; space : range list
+    }
+
+  and range = Digest.t * Digest.t
+
+  val of_file : Path.t -> (t, string) Result.t
+end
+
 val make : Uri.t -> Cache.Local.t -> (module Distributed.S)
